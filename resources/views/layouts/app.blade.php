46d0b050c2ea17cc7eea15136c51956c693b9ab6
aljorypro <b_pro@hotmail.com>
2022-10-24 22:45:09 +0300

    initial release

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
   <title>@yield('title')</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  @vite('resources/css/app.css')
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
    <body class="antialiased">
