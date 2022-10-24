95a0d9e490bf524ced7430aa305b2484dc42e140
aljorypro <b_pro@hotmail.com>
2022-10-24 23:29:23 +0300

    initial release

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,21 @@
   <title>@yield('title')</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+
+  {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+
+  @production
+    @php
+        $manifest = json_decode(file_get_contents(public_path('build/manifest.json')), true);
+    @endphp
+    <script type="module" src="/build/{$manifest['resources/js/app.js']['file']}"></script>
+    <link rel="stylesheet" href="/build/{$manifest['resources/js/app.js']['css'][0]}">
+@else
+    <script type="module" src="http://localhost:3000/@vite/client"></script>
+    <script type="module" src="http://localhost:3000/resources/js/app.js"></script>
+@endproduction
+
 </head>
 
    <body class="antialiased">
